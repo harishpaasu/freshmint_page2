6c3c5c2641147dec098d1f85841be4b4b630a94b
harish paasu <paasuharish6@gmail.com>
2024-03-03 19:26:58 +0530

    page-2

---
+++ freshmint-pg2/src/App.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './App.css'
+import StarkconImage from'./assets/starkcon.png' 
+
+function App() {
+  
+
+  return (
+    <><div class="Rectangle-18">
+    <span className="Ahoy">Ahoy</span>
+  <span className="Events">Events</span>
+<span className="Community">Community</span>
+<span className="Profiles">Profiles</span>
+<span className="Connect"> <button className="button-1" type='button'>Connect </button> </span>
+<span className="Event-1">Events</span>
+<span className="frame-2"> <button className='button-2' type="button">Create Event </button></span>
+
+<div className='flex-container'>
+<div className='box-1'>
+<img className="" src={"StarkconImage"} alt='starkcon'/>
+<div class="Rectangle-23">
+<div className="StarkCon-IIT-Madras">StarkCon IIT Madras</div>
+<div className="IITM-Research-Park">IITM Research Park</div>
+<div className="The-future-of-Web3-The-StarkWare-ZKX-Community-building-in-Web3">
+  The future of Web3, The StarkWare & ZKX Community building in Web3.</div>
+  <div className="Mangae-Event">Mangae Event</div>
+  </div>
+
+  <div className='box-2'>
+<img className="" src={"StarkconImage"} alt='starkcon'/>
+<div class="Rectangle-23">
+<div className="StarkCon-IIT-Madras">StarkCon IIT Madras</div>
+<div className="IITM-Research-Park">IITM Research Park</div>
+<div className="The-future-of-Web3-The-StarkWare-ZKX-Community-building-in-Web3">
+  The future of Web3, The StarkWare & ZKX Community building in Web3.</div>
+  <div className="Mangae-Event">Mangae Event</div>
+  </div>
+  </div>
+
+  </div>
+
+</div>
+
+ 
+
+
+
+
+</div>
+      
+    </>
+  )
+}
+
+export default App
