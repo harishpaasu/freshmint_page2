6c58372bdc300279bef09f8e0453317ad44b5583
harish paasu <paasuharish6@gmail.com>
2024-03-04 14:51:39 +0530

    change in design

--- freshmint-pg2/src/App.jsx
+++ freshmint-pg2/src/App.jsx
@@ -1,55 +1,69 @@
-import React from 'react'
-import './App.css'
-import StarkconImage from'./assets/starkcon.png' 
-
+import './App.css';
+import StarkconImage from './assets/starkcon.png';
+import ellipse from "./assets/Ellipse 7.png";
 function App() {
-  
-
   return (
-    <><div class="Rectangle-18">
-    <span className="Ahoy">Ahoy</span>
-  <span className="Events">Events</span>
-<span className="Community">Community</span>
-<span className="Profiles">Profiles</span>
-<span className="Connect"> <button className="button-1" type='button'>Connect </button> </span>
-<span className="Event-1">Events</span>
-<span className="frame-2"> <button className='button-2' type="button">Create Event </button></span>
-
-<div className='flex-container'>
-<div className='box-1'>
-<img className="" src={"StarkconImage"} alt='starkcon'/>
-<div class="Rectangle-23">
-<div className="StarkCon-IIT-Madras">StarkCon IIT Madras</div>
-<div className="IITM-Research-Park">IITM Research Park</div>
-<div className="The-future-of-Web3-The-StarkWare-ZKX-Community-building-in-Web3">
-  The future of Web3, The StarkWare & ZKX Community building in Web3.</div>
-  <div className="Mangae-Event">Mangae Event</div>
+    <div className="Rectangle-18">
+      <div className="container">
+  <div className="horizontal-container">
+    <nav>
+      <ul className="navigation">
+        
+        <li><a href="#" className="Ahoy">Ahoy</a></li>
+      </ul>
+    </nav>
+    <nav className="right-nav">
+      <ul className="navigation">
+        <li><a href="#" className="Events">Events</a></li>
+        <li><a href="#" className="Community">Community</a></li>
+        <li><a href="#" className="Profiles">Profiles</a></li>
+        <li><button className="button-1" type='button'>Connect</button></li>
+      </ul>
+    </nav>
   </div>
 
-  <div className='box-2'>
-<img className="" src={"StarkconImage"} alt='starkcon'/>
-<div class="Rectangle-23">
-<div className="StarkCon-IIT-Madras">StarkCon IIT Madras</div>
-<div className="IITM-Research-Park">IITM Research Park</div>
-<div className="The-future-of-Web3-The-StarkWare-ZKX-Community-building-in-Web3">
-  The future of Web3, The StarkWare & ZKX Community building in Web3.</div>
-  <div className="Mangae-Event">Mangae Event</div>
-  </div>
+  <div className="horizontal-container">
+    <h1 className="Event-1">Events</h1>
+    <div className="frame-2">
+      <button className='button-2' type="button">Create Event</button>
+    </div>
   </div>
 
-  </div>
+  <div className='flex-container'>
+    <div className='card'>
+      <img className='card-img1'src={StarkconImage} alt='starkcon'/>
+      <div className="card-content">
+        <h2>StarkCon IIT Madras</h2>
+        <p>IITM Research Park</p>
+        <p>The future of Web3, The StarkWare & ZKX Community building in Web3.</p>
+        <img className='card-img2' src={ellipse} alt='ellipse'/>
+        <img className='card-img3' src={ellipse} alt='ellipse'/>
+        <img className='card-img4' src={ellipse} alt='ellipse'/>
+        <img className='card-img5' src={ellipse} alt='ellipse'/>
+        <p className='card-p'>Manage Event</p>
+      </div>
+    </div>
 
+    <div className='card'>
+      <img className='card-img1' src={StarkconImage} alt='starkcon'/>
+      <div class="box"></div>
+      <div className="card-content">
+        <h2>StarkCon IIT Madras</h2>
+        <p>IITM Research Park</p>
+        <p>The future of Web3, The StarkWare & ZKX Community building in Web3.</p>
+        <div className='card-img11'
+        ><img className='card-img2' src={ellipse} alt='ellipse'/>
+        <img className='card-img3' src={ellipse} alt='ellipse'/>
+        <img className='card-img4' src={ellipse} alt='ellipse'/>
+        <img className='card-img5' src={ellipse} alt='ellipse'/>
+        <p className='card-p'>Manage Event</p></div>
+      </div>
+    </div>
+  </div>
 </div>
 
- 
-
-
-
-
-</div>
-      
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
